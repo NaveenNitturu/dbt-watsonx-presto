cb98f509c36f6f5a94143644a61afc68100c6787
KNagaVivek <Karnati.Naga.Vivek@ibm.com>
2024-12-06 18:41:27 +0530

    Add datatypes support for dbt seed

--- dbt/include/presto/macros/adapters.sql
+++ dbt/include/presto/macros/adapters.sql
@@ -81,6 +81,111 @@
   {{ return(sql) }}
 {% endmacro %}
 
+
+{% macro generate_bindings(row_values, column_types) %}
+  {% set formatted_values = [] %}
+
+  {%- for value in row_values -%}
+    {%- if value is not none -%}
+      {% set regex_module = modules.re %}
+      {%- set matched_type = regex_module.match("(\w+)(\(.*\))?", column_types[loop.index0]) -%}
+      {%- set type_category = matched_type.group(1).lower() -%}
+
+      {% if 'interval' in type_category %}
+        {%- do formatted_values.append((none, type_category.upper() ~ " " ~ value)) -%}
+      {% elif 'hyperloglog' in type_category or 'p4hyperloglog' in type_category or 'khyperloglog' in type_category %}
+        {%- do formatted_values.append((none, "CAST(" ~ value ~ " AS " ~ type_category.upper() ~ ")")) -%}
+      {% elif 'array' in type_category or 'map' in type_category or 'row' in type_category %}
+        {%- do formatted_values.append((none, type_category.upper() ~ value.strip("'\""))) -%}
+      {% elif 'varchar' in type_category or 'char' in type_category %}
+        {%- do formatted_values.append((get_binding_char(), value|string())) -%}
+      {% elif value is string %}
+        {%- do formatted_values.append((none, type_category.upper() ~ " '" ~ value.strip("'\"") ~ "'")) -%}
+      {% else %}
+        {%- do formatted_values.append((get_binding_char(), value)) -%}
+      {% endif %}
+    {% else %}
+      {%- do formatted_values.append((get_binding_char(), value)) -%}
+    {% endif %}
+  {%- endfor %}
+
+  {{ return(formatted_values) }}
+{% endmacro %}
+
+
+{#
+  Macro Purpose:
+  --------------
+  This macro is designed to load CSV data into a Presto table, ensuring that each value is cast to the appropriate
+  data type based on either automatically detected types or user-specified types.
+
+  Why This Macro is Needed:
+  -------------------------
+  - Presto requires explicit data type casting for certain values during insertion. For example, when inserting
+    CHAR, VARCHAR, or JSON types, the values must be formatted correctly.
+  - Typically, dbt detects the data types from the CSV data automatically, but dbt also allows the user to override
+    the column types via the `column_types` setting in the `dbt_project.yml` file.
+
+  Macro Workflow:
+  ---------------
+  1. The macro reads the data from the CSV file and detects the data types of each column.
+  2. If `column_types` is provided in `dbt_project.yml`, the macro applies the custom types to the columns.
+  3. It ensures proper formatting for each value (e.g., casting strings as CHAR, handling JSON objects, etc.).
+  4. The formatted values are then inserted into the Presto table, ensuring compliance with Presto’s data type
+     requirements.
+
+  Benefit:
+  --------
+  This macro ensures that the seed data from CSV files is accurately loaded into Presto, with each column having the
+  correct data type whether it is automatically detected or manually specified.
+#}
+
+{% macro presto__load_csv_rows(model, agate_table) %}
+  {% set column_override = model['config'].get('column_types', {}) %}
+  {% set types = [] %}
+
+  {%- for col_name in agate_table.column_names -%}
+      {%- set inferred_type = adapter.convert_type(agate_table, loop.index0) -%}
+      {%- set type = column_override.get(col_name, inferred_type) -%}
+      {%- do types.append(type) -%}
+  {%- endfor -%}
+
+  {% set batch_size = get_batch_size() %}
+
+  {% set cols_sql = get_seed_column_quoted_csv(model, agate_table.column_names) %}
+  {% set statements = [] %}
+
+  {% for chunk in agate_table.rows | batch(batch_size) %}
+      {% set bindings = [] %}
+
+      {% set sql %}
+          insert into {{ this.render() }} ({{ cols_sql }}) values
+          {% for row in chunk -%}
+              ({%- for formatted_value in generate_bindings(row, types) -%}
+                  {%- if formatted_value.0 is not none  -%}
+                  {{ formatted_value.0 }}
+                  {%- do bindings.append(formatted_value.1) -%}
+                  {%- else -%}
+                  {{ formatted_value.1 }}
+                  {%- endif -%}
+                  {%- if not loop.last%},{%- endif %}
+              {%- endfor -%})
+              {%- if not loop.last%},{%- endif %}
+          {%- endfor %}
+      {% endset %}
+
+      {% do adapter.add_query(sql, bindings=bindings, abridge_sql_log=True) %}
+
+      {% if loop.index0 == 0 %}
+          {% do statements.append(sql) %}
+      {% endif %}
+  {% endfor %}
+
+  {# Return SQL so we can render it out into the compiled files #}
+  {{ return(statements[0]) }}
+{% endmacro %}
+
+
 {% macro properties(properties) %}
   {%- if properties is not none -%}
       WITH (
